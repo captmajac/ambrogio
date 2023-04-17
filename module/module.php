9b0646b62bb7c58c6fb4d5edcb327b7c7749509b
captmajac <36032330+captmajac@users.noreply.github.com>
2023-04-17 10:53:48 +0200

    Update module.php

--- module/module.php
+++ module/module.php
@@ -29,7 +29,9 @@ class Ambrogio {
   
   // send go online message
 	private function goOnline($data) { 
-	                                      
+	$key = $this->ReadPropertyString("ThingKey");
+    $jsonDataEncoded = '{"0" : {"params" : {"coding" : "SEVEN_BIT", "imei" : "'.$key.'","message" : "UP"},"command" : "sms.send"}}';
+    obj = sendCloudMessage($jsonDataEncoded);                        
 	}
 	
    	// send getStatus message
@@ -41,6 +43,66 @@ class Ambrogio {
 	
   // send cloud message
 	private function sendCloudMessage($data) { 
+		$user = $this->ReadPropertyString("User");
+		$pass = $this->ReadPropertyString("Pass");
+		$key = $this->ReadPropertyString("ThingKey");
+		$sessionid = $this->ReadPropertyString("SessionID");
+		
+		
+	//The URL you're sending the request to.
+    $url = 'http://api-de.devicewise.com/api';
+
+    //Create a cURL handle.
+    $ch = curl_init($url);
+
+    //Create an array of custom headers.
+    $customHeaders = array(
+        'Host: api-de.devicewise.com',
+        'Connection: keep-alive',
+        'Content-Type: application/json',
+        'Accept: application/json',
+        'User-Agent: Ambrogio%20Remote/56970 CFNetwork/1331.0.7 Darwin/21.4.0',
+        'Accept-Language: de-DE,de;q=0.9',
+        'Accept-Encoding: gzip, deflate'
+        );
+
+    // Auth session ID oder user/pass
+    if ($sessionID<>"")
+    {
+        array_push($customHeaders, "sessionId:".$sessionID);
+    }
+    else
+    {
+        // auth header
+        curl_setopt($ch, CURLOPT_USERPWD, $user.":".$pass);
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    }
+
+    //Use the CURLOPT_HTTPHEADER option to use our
+    //custom headers.
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $customHeaders);
+
+    //Attach our encoded JSON string to the POST fields.
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+
+    // returns the result - very important
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    //Tell cURL that we want to send a POST request.
+    curl_setopt($ch, CURLOPT_POST, 1);
+    //Execute the request.
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    echo $result."\n";
+    $obj = json_decode($result);
+
+    if ($obj->{'errorCodes'}[0] = -99999 )
+    {
+        echo "Authentication header Fehler" ; // Login und dann Session ID notwendig.
+    }
+
+    return $obj;		
 	                                      
 	}
 
