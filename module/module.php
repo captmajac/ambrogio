13d685462b5ec2306cb94f5642bcb0f61d853482
captmajac <36032330+captmajac@users.noreply.github.com>
2023-04-17 10:48:53 +0200

    Update module.php

--- module/module.php
+++ module/module.php
@@ -7,6 +7,7 @@ class Ambrogio {
 		parent::Create ();
 		$this->RegisterPropertyString ( "User", "" );
     $this->RegisterPropertyString ( "Pass", "" );
+	$this->RegisterPropertyString ( "ThingKey", "" );
     $this->RegisterPropertyString ( "SessionID", "" );
 		
 	}
@@ -22,14 +23,22 @@ class Ambrogio {
 	
 	// login cloud
 	private function loginCloud($data) { 
-	                                      
+		$jsonDataEncoded = '{"auth":{"command":"api.authenticate","params":{"appId":"3c1Pt1We9dT3qBAlL7nxAcDERC82","thingKey":"3c1Pt1We9dT3qBAlL7nxAcDERC82","appToken":"DJMYYngGNEit40vA"}}}';
+		$obj = sendCloudMessage($jsonDataEncoded);                              
 	}
   
   // send go online message
 	private function goOnline($data) { 
 	                                      
 	}
-  
+	
+   	// send getStatus message
+	private function getStatus($data) {  
+		$key = $this->ReadPropertyString("ThingKey");
+		$jsonDataEncoded = '{"state_history":{"command":"alarm.history","params":{"thingKey":"'.$key.'","key":"robot_state","last":"24h"}},"thing_find":{"command":"thing.find","params":{"key":"'.$key.'"}}}';
+		$obj = sendCloudMessage($jsonDataEncoded);
+	}
+	
   // send cloud message
 	private function sendCloudMessage($data) { 
 	                                      
