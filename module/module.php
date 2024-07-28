6af4cb0203fd457e89dbf82f6d9afa2da3714ce6
captmajac <36032330+captmajac@users.noreply.github.com>
2024-07-28 15:59:00 +0200

    Update module.php

--- module/module.php
+++ module/module.php
@@ -58,6 +58,7 @@ class Ambrogio extends IPSModule
         }
         $this->SetTimerInterval("UpdateTimer", $Interval);
 
+	    echo ($this->ReadPropertyString("MapID"));
     	if ($this->ReadPropertyString("MapID") != "")
 	{
 		IPS_SetHidden($this->GetIDForIdent("Map"),false);
